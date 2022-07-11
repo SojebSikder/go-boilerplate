0f3b9cad0de0cdb02500a7e1fa3472f3c9661892
sojebsikder <sojebsikder@gmail.com>
2022-07-11 12:10:26 +0600

    main

--- routes/web.go
+++ routes/web.go
@@ -1,11 +1,11 @@
 package routes
 
 import (
-	"github.com/SojebSikder/goframe/app/controller"
+	"github.com/SojebSikder/goframe/app/controller/example"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(router *gin.Engine) {
-	router.GET("/", controller.Index)
-	router.GET("/test", controller.Hello)
+	router.GET("/", example.Index)
+	router.GET("/test", example.Hello)
 }
