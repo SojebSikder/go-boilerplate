6c5d07840e4fcfa12b1e7aed3cf23b3408b481f4
sojebsikder <sojebsikder@gmail.com>
2025-10-11 14:51:32 +0600

    refactor: refactor docker-compose.yml

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/sojebsikder/go-boilerplate/internal/config"
 	"github.com/sojebsikder/go-boilerplate/internal/model"
 	"github.com/sojebsikder/go-boilerplate/internal/modules/app/auth"
@@ -8,6 +11,7 @@ import (
 	"github.com/sojebsikder/go-boilerplate/internal/routes"
 	"github.com/sojebsikder/go-boilerplate/pkg/ORM"
 	"github.com/sojebsikder/go-boilerplate/pkg/repository"
+	"github.com/spf13/cobra"
 	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
@@ -17,18 +21,41 @@ import (
 	"log"
 )
 
-func GinServer() *gin.Engine {
-	r := gin.Default()
-	r.Static("/static", "./"+config.StaticDir)
-	r.LoadHTMLGlob(config.TemplateDir + "/*")
-	return r
+var verbose bool
+
+var RootCmd = &cobra.Command{
+	Use:   "mycli",
+	Short: "MyCLI is a demo CLI tool",
+	Long:  "MyCLI is a demonstration of a complex CLI using Cobra in Go.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Use --help to see available commands.")
+	},
 }
 
-func AutoMigrate(db *gorm.DB) {
-	db.AutoMigrate(&model.User{})
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start the web server",
+	Run: func(cmd *cobra.Command, args []string) {
+		StartServer()
+	},
+}
+
+// Execute runs the CLI
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func main() {
+	RootCmd.AddCommand(serverCmd)
+	RootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
+
+	Execute()
+}
+
+func StartServer() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -49,5 +76,15 @@ func main() {
 		),
 	)
 	app.Run()
+}
 
+func GinServer() *gin.Engine {
+	r := gin.Default()
+	r.Static("/static", "./"+config.StaticDir)
+	r.LoadHTMLGlob(config.TemplateDir + "/*")
+	return r
+}
+
+func AutoMigrate(db *gorm.DB) {
+	db.AutoMigrate(&model.User{})
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
 	github.com/joho/godotenv v1.5.1
+	github.com/spf13/cobra v1.10.1
 	go.uber.org/fx v1.24.0
 	golang.org/x/crypto v0.40.0
 	gorm.io/driver/postgres v1.6.0
@@ -25,6 +26,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
@@ -39,6 +41,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/dig v1.19.0 // indirect
